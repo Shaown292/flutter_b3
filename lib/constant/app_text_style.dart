397dad8942661d39e2499389849878105035c723
kzshaown <kzshaown@gmai,.com>
2025-07-19 21:06:17 +0600

    init

---
+++ lib/constant/app_text_style.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class AppTextStyle {
+  static TextStyle textStyle52BlackW700 = GoogleFonts.raleway(
+    fontSize: 52,
+    color: Color(0xFF202020),
+    fontWeight: FontWeight.w700,
+  );
+
+  static TextStyle textStyle30BlackW400 = GoogleFonts.abyssinicaSil(
+    fontSize: 30,
+    color: Color(0xFF000000),
+    fontWeight: FontWeight.w400,
+  );
+  static TextStyle textStyle25BlackW400 = GoogleFonts.abyssinicaSil(
+    fontSize: 25,
+    color: Color(0xFF000000),
+    fontWeight: FontWeight.w400,
+  );
+  static TextStyle textStyle16BWhiteW400 = GoogleFonts.abyssinicaSil(
+    fontSize:16,
+    color: Color(0xFFFFFFFF),
+    fontWeight: FontWeight.w400,
+  );
+  static TextStyle textStyle17GreyW400 = GoogleFonts.abyssinicaSil(
+    fontSize:17,
+    color: Color(0xFF8E8E93),
+    fontWeight: FontWeight.w400,
+  );
+  static TextStyle randomText = GoogleFonts.inter(
+      fontSize: 30,
+      fontWeight: FontWeight.w500
+  );
+
+}
